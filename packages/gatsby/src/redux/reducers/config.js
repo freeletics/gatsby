bb9db33ed603a6405b4695cbef74d85a9f237bc5
Szymon Nowak <szimek@gmail.com>
2019-01-29 19:21:09 +0100

    Add assetPath option to config reducer

--- packages/gatsby/src/redux/reducers/config.js
+++ packages/gatsby/src/redux/reducers/config.js
@@ -4,6 +4,11 @@ const _ = require(`lodash`)
 
 const { gatsbyConfigSchema } = require(`../../joi-schemas/joi`)
 
+const ensureStartsWithSlash = path =>
+  !_.startsWith(path, `/`) ? `/${path}` : path
+const ensureEndsWithNoSlash = path =>
+  _.endsWith(path, `/`) ? path.slice(0, -1) : path
+
 module.exports = (state = {}, action) => {
   switch (action.type) {
     case `SET_SITE_CONFIG`: {
@@ -26,18 +31,35 @@ module.exports = (state = {}, action) => {
       // Ensure that the pathPrefix (if set) starts with a forward slash
       // and doesn't end with a slash.
       if (action.payload && action.payload.pathPrefix) {
-        if (!_.startsWith(action.payload.pathPrefix, `/`)) {
-          action.payload.pathPrefix = `/${action.payload.pathPrefix}`
-        }
-        if (_.endsWith(action.payload.pathPrefix, `/`)) {
-          action.payload.pathPrefix = action.payload.pathPrefix.slice(0, -1)
-        }
+        action.payload.pathPrefix = ensureStartsWithSlash(
+          action.payload.pathPrefix
+        )
+        action.payload.pathPrefix = ensureEndsWithNoSlash(
+          action.payload.pathPrefix
+        )
+      }
+
+      // Ensure that the assetPath (if set) starts with a forward slash
+      // and doesn't end with a slash.
+      if (action.payload && action.payload.assetPath) {
+        action.payload.assetPath = ensureStartsWithSlash(
+          action.payload.assetPath
+        )
+        action.payload.assetPath = ensureEndsWithNoSlash(
+          action.payload.assetPath
+        )
       }
 
       // If pathPrefix isn't set, set it to an empty string
       // to avoid it showing up as undefined elsewhere.
       if (!_.has(action, [`payload`, `pathPrefix`])) {
         action = _.set(action, [`payload`, `pathPrefix`], ``)
+      }
+
+      // If assetPath isn't set, set it to an empty string
+      // to avoid it showing up as undefined elsewhere.
+      if (!_.has(action, [`payload`, `assetPath`])) {
+        action = _.set(action, [`payload`, `assetPath`], ``)
       }
 
       // Default polyfill to true.
